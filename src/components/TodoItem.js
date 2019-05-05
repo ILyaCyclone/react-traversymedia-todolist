af4b91547efb1f2e149b41abf464152a73a4ec98
ILya Cyclone <ilya.cyclone@gmail.com>
2019-05-06 00:41:30 +0300

    style for todo item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+    itemStyle = () => {
+        return {
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none',
+            backgroundColor: "#f4f4f4",
+            padding: '10px',
+            border: "1px #ccc solid"
+        }
+    } 
+
+
   render() {
     return (
-      <div>
+      <div style={this.itemStyle()}>
         {this.props.todo.title}
       </div>
     )
   }
 }
+
 
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
